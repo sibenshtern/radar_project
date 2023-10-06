1fc428d0ed17ce8b222374d09f5b68e9e7c7bc76
drrkkm <vikei.fafg@yandex.ru>
2023-10-06 12:07:19 +0300

    prepare for merge branches

---
+++ collision_detector.py
@@ -0,0 +1,7 @@
+
+
+class CollisionDetector:
+    def __init__(self):
+        pass
+    def calculate(self, objects, signals, radar): list:
+        pass
--- modeling.py
+++ modeling.py
@@ -8,13 +8,19 @@ import datetime
 
 
 class Scene:
-    def __init__(self, radar: Radar, objects : list[AerialObject], signals: list[Signal]):
+    def __init__(self, radar: Radar, objects : list[AerialObject], signals: list[Signal], duration: int):
         self.radar = Radar
         self.objects: list = objects
         self.signals: list = signals
+        self.time: int = 0
+        self.duration: int = duration
+
+    send_signal(self, angle: int, departure_time: int, direction: Coordinates,
+                 speed: Coordinates):
 
     def update(self):
-        signal = self.radar.emitter.send_signal(0, self.convert_time(now.time()), , )
+        self.time += 1
+        signal = self.radar.emitter.send_signal(0, self.time, , )
 
         angle: int, departure_time: int, direction: Coordinates,
                  speed: Coordinates
