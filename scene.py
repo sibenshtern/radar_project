5c4d7dfe5c9d48ee16218b9a693cc9c193d8b9ec
Ivanov Egor <sibenshtern@gmail.com>
2023-11-09 18:30:56 +0300

    upload code for testing

--- scene.py
+++ scene.py
@@ -3,9 +3,10 @@ from model import *
 from radar import Radar
 from objects.aircraft import Aircraft
 from signal import Signal
+from copy import deepcopy
 # from collision_detector import CollisionDetector
 # from tracker import Tracker
-from coordinates.coordinates import Vector3D
+from coordinates.coordinates import Coordinates3D
 
 import pygame as pg
 
@@ -34,13 +35,13 @@ class Scene:
         add = self.add_object
 
         # radar station
-        # add(Cube(app))
+        add(Cube(app, pos=(1, 0, 0)))
 
         # ground
         n, s = 30, 2
         for x in range(-n, n, s):
             for y in range(-n, n, s):
-                add(Cube(app, pos=(x, -y, -10), tex_id=1))
+                add(Cube(app, pos=(-x, -2, y), tex_id=1))
 
     def send_signals(self):
         self.signals.extend(self.radar.emitter.send_signals(self.time))
@@ -48,8 +49,18 @@ class Scene:
         self.reflected.append([signal.reflected for signal in self.signals])
 
     def render(self):
+        # print("QQQQQQ")
         for obj in self.objects:
+            # right_position = deepcopy(obj.position)
+            # new_position = None
+            # if isinstance(obj.position, tuple):
+            #     new_position = (right_position[0], right_position[2], right_position[1])
+            # elif isinstance(obj.position, Coordinates3D):
+            #     new_position = Coordinates3D(right_position.x, right_position.z, right_position.y)
+
+            # obj.position = new_position
             obj.render()
+            # obj.position = right_position
         # signals_detection_object = self.collision_detector.scan_objects(self.signals, self.objects, self.time)
 
         # for signal in signals_detection_object:
