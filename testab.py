6e736b5f60a2258efbed24d6dd2085bbf43fc814
drrkkm <vikei.fafg@yandex.ru>
2023-11-10 15:52:58 +0300

    test abfilter

--- testab.py
+++ testab.py
@@ -1,5 +1,22 @@
-import
+import abfilter
+import objects
+from coordinates import Coordinates3D
 
-ab = ABFilter()
+ab = abfilter.ABFilter()
 
-ab.
+
+obj = objects.tracked.Tracked(0)
+ab.filter(obj, Coordinates3D(1, 1, 1))
+ab.filter(obj, Coordinates3D(2, 2, 2))
+ab.filter(obj, Coordinates3D(3, 3, 3))
+
+
+#ab.filter(obj, Coordinates3D(1, 1, 1))
+#ab.filter(obj, Coordinates3D(1, 1, 1))
+#ab.filter(obj, Coordinates3D(1, 1, 1))
+
+print(obj.extrapolatedValue)
+
+ab.filter(obj, Coordinates3D(4.1, 3.9, 4))
+
+print(obj.trajectory)
